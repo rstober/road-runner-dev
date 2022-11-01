8e6030577e41c3a3101016ac7eba64390051abf5
Robert Stober <rstober@users.noreply.github.com>
2022-11-01 15:11:22 -0800

    Add files via upload

--- install.py
+++ install.py
@@ -167,7 +167,7 @@ if __name__ == '__main__':
     
         for image in dictionary["software_images"]:
         
-            pprint(image)
+            pp.pprint(image)
             sys.exit("exiting")
 
             initrd_file = '/cm/images/' + image["name"] + '/boot/initrd-' + image["kernel_release"]
