9580cc9754cd3c5e479aca9217c41148c5c82ac8
Robert Stober <rstober@users.noreply.github.com>
2022-11-06 16:58:53 -0900

    Add files via upload

--- install.py
+++ install.py
@@ -174,8 +174,6 @@ if __name__ == '__main__':
             
             os.system('ansible-playbook -ilocalhost, -v --extra-vars "index={index} image_name={image_name} clone_from={clone_from} image_path={image_path} kernel_release={kernel_release} image_backup={image_backup}" create-software-image-pb.yaml'.format(index=index, image_name=image["name"], clone_from=image["clone_from"], image_path=image["path"], kernel_release=image["kernel_release"], image_backup=image["backup"]))
             
-            sys.exit("exiting")
-            
             # skip adding kernel modules if there are none to add
             if image["modules"] is not None:
             
