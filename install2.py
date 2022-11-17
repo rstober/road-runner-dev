965a808e92f2fceccdb82270960c3752f88a60d4
Robert Stober <rstober@users.noreply.github.com>
2022-11-17 08:38:23 -0900

    Add files via upload

--- install2.py
+++ install2.py
@@ -15,7 +15,6 @@ import logging
 import sys
 
 install_dir = "/root/.road-runner"
-tmp_dir = install_dir + '/tmp'
 begin_time = datetime.datetime.now()
 
 logger = logging.getLogger()
@@ -108,9 +107,6 @@ if __name__ == '__main__':
     # os.chmod("aws/install", stat.S_IEXEC)
     # os.chmod("aws/dist/aws", stat.S_IEXEC)
     
-    # create the tmp directory
-    createDirectoryPath(tmp_dir)
-    
     # load the python3 module
     exec(open('/cm/local/apps/environment-modules/4.5.3/Modules/default/init/python.py').read())
     os.environ['MODULEPATH'] = '/cm/local/modulefiles:/cm/shared/modulefiles'
@@ -189,32 +185,6 @@ if __name__ == '__main__':
             
             os.system('ansible-playbook -ilocalhost, -e "username={username} password={password}" add-user.yml'.format(username=user, password=password))
         
-    # if "packages" in config:
-    
-        # index=0
-        
-        # shutil.copyfile("default-ansible-vars", install_dir + "/roles/packages/vars/main.yaml")
-    
-        # for package in config["packages"]:
-        
-            # index +=1
-            
-            # if package["target"] == "headnode":
-            
-                # os.system('ansible-playbook -ilocalhost, --extra-vars "index={index} package_name={package_name}" headnode-install-package-pb.yaml'.format(index=index, package_name=package["package_name"]))
-                
-            # else:
-                # os.system('ansible-playbook -ilocalhost, --extra-vars "index={index} package_name={package_name} target={target}" node-install-package-pb.yaml'.format(index=index, package_name=package["package_name"], target=package["target"]))
-                
-                # if package["package_name"] == "cuda-driver":
-                
-                    # index+=1
-                    
-                    # os.system('ansible-playbook -ilocalhost, --extra-vars "index={index} target={target}" patch-cuda-driver-service-file-pb.yaml'.format(index=index, target=package["target"]))
-                    
-        # concatenateFiles(config["tmp_dir"], 'roles/packages/tasks/main.yaml')
-        # cleanTmpDir(config["tmp_dir"])
-        
     # if "kubernetes" in config:
     
         # index=0
@@ -229,127 +199,6 @@ if __name__ == '__main__':
         
         # concatenateFiles(config["tmp_dir"], 'roles/kubernetes/tasks/main.yaml')
         # cleanTmpDir(config["tmp_dir"])
-                    
-    # if "wlms" in config:
-    
-        # shutil.copyfile("bright-ansible-vars", install_dir + "/roles/wlms/vars/main.yaml")
-    
-        # for wlm in config["wlms"]:
-        
-            # if wlm["name"] == "slurm":
-            
-                # index=0
-            
-                # if wlm["constrain_devices"]:
-                
-                    # os.system('ansible-playbook -ilocalhost, --extra-vars "wlm_name={wlm_name} index={index}" configure-wlm-pb.yaml'.format(wlm_name=wlm["name"], index=index))
-            
-                # for queue in wlm["queues"]:
-            
-                    # index+=1
-                
-                    # os.system('ansible-playbook -ilocalhost, --extra-vars "index={index} queue_name={queue_name} clone_from={clone_from} default_queue={default_queue} over_subscribe={over_subscribe} wlm_cluster={wlm_cluster}" clone-slurm-queue-pb.yaml'.format(index=index, queue_name=queue["queue_name"], clone_from=queue["clone_from"], default_queue=queue["default_queue"], over_subscribe=queue["over_subscribe"], wlm_cluster=queue["wlm_cluster"]))
-            
-                # for overlay in wlm["configuration_overlays"]:
-            
-                    # index+=1
-                
-                    # os.system('ansible-playbook -ilocalhost, --extra-vars "index={index} overlay_name={overlay_name} categories={categories} all_head_nodes={all_head_nodes}" create-configuration-overlay-pb.yaml'.format(index=index, overlay_name=overlay["name"], categories=overlay["categories"], all_head_nodes=overlay["allHeadNodes"]))
-                
-                    # for role in overlay["roles"]:
-                
-                        # index+=1
-                    
-                        # os.system('ansible-playbook -ilocalhost, --extra-vars "index={index} overlay_name={overlay_name} role_name={role_name} wlm_cluster={wlm_cluster} queues={queues} sockets_per_board={sockets_per_board} cores_per_socket={cores_per_socket} threads_per_core={threads_per_core} slots={slots} real_memory={real_memory}" add-role-to-slurm-client-overlay-pb.yaml'.format(index=index, overlay_name=overlay["name"], role_name=role["name"], wlm_cluster=role["wlm_cluster"], queues=role["queues"], sockets_per_board=role["sockets_per_board"], cores_per_socket=role["cores_per_socket"], threads_per_core=role["threads_per_core"], slots=role["slots"], real_memory=role["real_memory"]))
-                    
-                        # for resource in role["generic_resources"]:
-                    
-                            # index+=1
-                    
-                            # os.system('ansible-playbook -ilocalhost, --extra-vars "index={index} overlay_name={overlay_name} role_name={role_name} resource_name={resource_name} alias={alias} file={file} res_type={res_type} count={count} consumable={consumable} add_to_gres_config={add_to_gres_config}" add-resource-pb.yaml'.format(index=index, overlay_name=overlay["name"], role_name=role["name"], resource_name=resource["name"], alias=resource["alias"], file=resource["file"], res_type=resource["type"], count=resource["count"], consumable=resource["consumable"], add_to_gres_config=resource["add_to_gres_config"]))
-            # else:                
-                # print("Error: unsupported workload management system")
-                # exit()
-                        
-        # concatenateFiles(config["tmp_dir"], 'roles/wlms/tasks/main.yaml')
-        # cleanTmpDir(config["tmp_dir"])
-                        
-    # if "autoscaler" in config:
-    
-        # index=0
-        # shutil.copyfile("bright-ansible-vars", install_dir + "/roles/autoscaler/vars/main.yaml")
-        
-        # os.system('ansible-playbook -ilocalhost, --extra-vars "index={index} overlay_name={overlay_name} categories={categories} all_head_nodes={all_head_nodes}" create-add-overlay-pb.yaml'.format(index=index, overlay_name=config["autoscaler"]["name"], categories=config["autoscaler"]["categories"], all_head_nodes=config["autoscaler"]["allHeadNodes"]))
-        
-        # for role in config["autoscaler"]["roles"]:
-        
-            # index+=1
-            
-            # os.system('ansible-playbook -ilocalhost, --extra-vars "index={index} role_name={role_name} runInterval={runInterval} debug={debug}" create-add-role-to-auto-scaler-overlay-pb.yaml'.format(index=index, role_name=role["name"], runInterval=role["runInterval"], debug=role["debug"]))
-            
-            # for provider in role["resource_providers"]:
-            
-                # index+=1
-            
-                # os.system('ansible-playbook -ilocalhost, --extra-vars "install_dir={install_dir} index={index} provider_name={provider_name} templateNode={templateNode} startTemplateNode={startTemplateNode} stopTemplateNode={stopTemplateNode} nodeRange={nodeRange} networkInterface={networkInterface} defaultResources={defaultResources}" add-dynamic-resource-provider-pb.yaml'.format(install_dir=config["install_dir"], index=index, provider_name=provider["provider_name"], templateNode=provider["templateNode"], startTemplateNode=provider["startTemplateNode"], stopTemplateNode=provider["stopTemplateNode"], nodeRange=provider["nodeRange"], networkInterface=provider["networkInterface"], defaultResources=provider["defaultResources"]))
-                
-            # for engine in role["engines"]:
-            
-                # index+=1
-               
-                # if engine["type"] == "ScaleHpcEngine":
-               
-                    # os.system('ansible-playbook -ilocalhost, --extra-vars "install_dir={install_dir} index={index} engine_name={engine_name} workloads_per_node={workloads_per_node} priority={priority} wlm_cluster={wlm_cluster}" add-ScaleHpcEngine-pb.yaml'.format(install_dir=config["install_dir"], index=index, engine_name=engine["name"], workloads_per_node=engine["workloadsPerNode"], priority=engine["priority"], wlm_cluster=engine["wlmCluster"]))
-               
-                # elif engine["type"] == "ScaleKubeEngine":
-               
-                    # os.system('ansible-playbook -ilocalhost, --extra-vars "install_dir={install_dir} index={index} engine_name={engine_name} workloads_per_node={workloads_per_node} priority={priority} cluster={cluster}" add-ScaleKubeEngine-pb.yaml'.format(install_dir=config["install_dir"], index=index, engine_name=engine["name"], workloads_per_node=engine["workloadsPerNode"], priority=engine["priority"], cluster=engine["cluster"]))
-                   
-                # else:
-                
-                    # print("Error: unsupported engine type")
-                    # exit()
-               
-                # for tracker in engine["trackers"]:
-               
-                    # index+=1
-                    
-                    # if tracker["type"] == "ScaleHpcQueueTracker":
-                   
-                        # os.system('ansible-playbook -ilocalhost, --extra-vars "install_dir={install_dir} index={index} tracker_name={tracker_name} queue={queue} assign_category={assign_category} allowed_resource_providers={allowed_resource_providers} workloads_per_node={workloads_per_node}" add-ScaleHpcQueueTracker.yaml'.format(install_dir=config["install_dir"], index=index, tracker_name=tracker["name"], queue=tracker["queue"], assign_category=tracker["assignCategory"], allowed_resource_providers=tracker["allowedResourceProviders"], workloads_per_node=tracker["workloadsPerNode"])) 
-                    
-                    # elif tracker["type"] == "ScaleKubeNamespaceTracker":   
-
-                        # os.system('ansible-playbook -ilocalhost, --extra-vars "install_dir={install_dir} index={index} tracker_name={tracker_name} controllerNamespace={controllerNamespace} assign_category={assign_category} allowed_resource_providers={allowed_resource_providers} workloads_per_node={workloads_per_node}" add-ScaleKubeNamespaceTracker.yaml'.format(install_dir=config["install_dir"], index=index, tracker_name=tracker["name"], controllerNamespace=tracker["controllerNamespace"], assign_category=tracker["assignCategory"], allowed_resource_providers=tracker["allowedResourceProviders"], workloads_per_node=tracker["workloadsPerNode"]))
-
-                    # else:
-                
-                        # print("Error: unsupported tracker type")
-                        # exit()                        
-   
-        # concatenateFiles(config["tmp_dir"], 'roles/autoscaler/tasks/main.yaml')
-        # cleanTmpDir(config["tmp_dir"])
-        
-    # if "csps" in config:
-        
-        # index=0
-        # shutil.copyfile("bright-ansible-vars", install_dir + "/roles/csps/vars/main.yaml")
-    
-        # for csp in config["csps"]:
-            
-            # if csp["type"] == "aws":
-            
-                # os.system('ansible-playbook -ilocalhost, --extra-vars "install_dir={install_dir} index={index} provider_name={provider_name} useMarketplaceAMIs={useMarketplaceAMIs}" config-ec2-csp.yaml'.format(install_dir=config["install_dir"], index=index, provider_name=csp["name"], useMarketplaceAMIs=csp["useMarketplaceAMIs"]))
-            
-            # else:
-            
-                # print("Error: Unsupported cloud service provider type")
-                # exit()
-                
-            # index+=1
-                
-        # concatenateFiles(config["tmp_dir"], 'roles/csps/tasks/main.yaml')
-        # cleanTmpDir(config["tmp_dir"])
         
     # if "jupyter" in config:
     
@@ -357,12 +206,6 @@ if __name__ == '__main__':
         
         # # write the playbook that installs Jupyter and opens port 8000 in the director security group
         # os.system('ansible-playbook -ilocalhost, install-jupyter-pb.yaml')
-        
-    # if "apps" in config:
-    
-        # shutil.copyfile("default-ansible-vars", install_dir + "/roles/apps/vars/main.yaml")
-        
-        # os.system('ansible-playbook -ilocalhost, install-apps-pb.yaml')
     
     printBanner('Run the playbooks')
     
